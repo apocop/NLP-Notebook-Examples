c3b24894ed04f980f461ca09b75dc28a46f95363
Daniel Dorado <37589053+apocop@users.noreply.github.com>
2020-01-06 19:22:41 -0800

    Code clean up.

--- 2019-12-23-lets-build-a-tokenizer/tokenizer.py
+++ 2019-12-23-lets-build-a-tokenizer/tokenizer.py
@@ -5,7 +5,7 @@ import re
 import exceptions
 import grammar
 
-class Tokenizer():
+class Tokenizer:
     """Tokenizer class."""
     def __init__(self):
         self.rules = grammar.RULES
@@ -13,15 +13,15 @@ class Tokenizer():
         self.exception_lexicon = exceptions.LEXICON
         self.accepted_tokens = None
 
-    def add_exception(self, exception):
+    def __add_exception(self, exception):
         """Add approved tokens from an acception list."""
         for token in self.exception_lexicon.get(exception):
             self.accepted_tokens.append(token)
 
-    def tokenize_pipeline(self, token):
+    def __tokenize_pipeline(self, token):
         """Return an approved token."""
         if token in self.exception_lexicon:
-            self.add_exception(token)
+            self.__add_exception(token)
         else:
             continue_matching = True
             rule_index = 0
@@ -32,7 +32,7 @@ class Tokenizer():
                 if match:
                     for group in match.groups():
                         if group:
-                            self.tokenize_pipeline(group)
+                            self.__tokenize_pipeline(group)
                             continue_matching = False
                 else:
                     if rule == self.rule_list[-1]:
@@ -46,6 +46,6 @@ class Tokenizer():
         self.accepted_tokens = []
         tokens = (token for token in string.split())
         for token in tokens:
-            self.tokenize_pipeline(token)
+            self.__tokenize_pipeline(token)
 
         return self.accepted_tokens
